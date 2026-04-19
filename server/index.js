4dcf13ce265c741b62eff7f13c0635747589cafc
Vaibhav Jadhav <vaibhavjadhav0391@gmail.com>
2026-04-19 13:49:53 +0530

    Update port bindings for Render and add start script

--- server/index.js
+++ server/index.js
@@ -29,5 +29,5 @@ io.on('connection', (socket) => {
   socket.on('disconnect', () => {});
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`Socket.io server running on port ${PORT}`);
 });
